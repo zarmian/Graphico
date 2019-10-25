3e018daee6ca866e8046ca61cc2f274c6eb33a0f
zarmian <zarmian@gmail.com>
2019-10-25 18:43:05 +0500

    initial commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<head>
+<link rel='stylesheet' id='main-style-css'  href='/assets/css/styles-ver=4.9.3.css' type='text/css' media='all' />
+<link rel='stylesheet' id='datepicker-css-css'  href='/css/bootstrap-datetimepicker.min-ver=4.9.3.css' type='text/css' media='all' />
+<link rel='stylesheet' id='bootstrap-css'  href='/css/bootstrap-header.css' type='text/css' media='all' />
+<link rel='stylesheet' id='font-icon-css'  href='/assets/css/font-awesome.min-ver=1.8.10.css' type='text/css' media='all' />
+
+    <title>@yield('title','GraphiCo')</title>
+</head>
+    <body class="home page-template page-template-page-home-new page-template-page-home-new-php page page-id-1617 two-column right-sidebar">
+        @include('layouts.header')
+
+        @if(session('success'))
+        <div class="alert alert-success">
+            <div class="container">
+                <span>
+                    {{ session('success') }}
+                </span>
+                <button type="button" class="close" data-dismiss="alert">
+                    <span aria-hidden="true">×</span>
+                    <span class="sr-only">Close</span>
+                </button>
+            </div>
+        </div>
+        @endif
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <div class="container">
+                <span>
+                    @foreach ($errors->all() as $error)
+                    {{ $error }}
+                    @endforeach
+                </span>
+                <button type="button" class="close" data-dismiss="alert">
+                    <span aria-hidden="true">×</span>
+                    <span class="sr-only">Close</span>
+                </button>
+            </div>
+        </div>
+	    @endif
+
+        @yield('content')
+        <script type="text/javascript" src="/aecore/assets/js/bootstrap.min-ver=1.8.10.js"></script>
+    <script type="text/javascript" src="/assets/js/bootstrap-datetimepicker.min-ver=1.8.10.js"></script>
+    <script type="text/javascript" src="/assets/js/moment.min-ver=1.8.10.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    @yield('scripts')
+    </body>
+
+    
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/admin', function(){
+    echo "Hello Admin";
+})->middleware('admin');
+ 
+Route::get('/Freelancer', function(){
+    return view('freelancer');
+})->middleware('Freelancer');
+ 
+Route::get('/client', function(){
+    return view('client');
+})->middleware('client');
+
+Auth::routes();
+Route::get('/', 'HomeController@index');
+Route::get('/home', 'HomeController@index');
+Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/posted', 'ProjectsController@posted');
+Route::get('/projects/create', 'ProjectsController@create');
+Route::get('/projects/myproject', 'ProjectsController@myproject');
+Route::get('project/competition/create', 'CompetitionController@create');
+Route::get('/download/competition/{file}', 'CompetitionController@download');
+Route::get('/download/project/{file}', 'ProjectsController@download');
+Route::get('/download/submit/{file}', 'WorkspaceController@download');
+Route::get('/projects/workspace/{project}', 'ProjectsController@workspace');
+
+Route::patch('project/{project}/archive','Projectscontroller@archive');
+Route::patch('project/{project}/active','Projectscontroller@active');
+Route::patch('project/{project}/bid','Projectscontroller@bid');
+Route::post('project/{project}/assign/{u_id}','Projectscontroller@assign');
+
+
+Route::post('post-project', 'ProjectsController@store')->name('store');
+Route::post('post-competition', 'CompetitionController@store')->name('store');
+Route::patch('/submit-project/{project}', 'ProjectsController@submit')->name('submit');
+
+Route::resource('projects','ProjectsController');
